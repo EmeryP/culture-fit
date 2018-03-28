600afcd0b2aa285b3dee9b1e06170541f16a30dc
Amy Cohen <anbcohen@gmail.com>
2018-03-28 11:45:16 -0700

    added functionality to refresh form button

--- js/form.js
+++ js/form.js
@@ -134,25 +134,21 @@ function storingUserAnswers (){
 fromLocalStorage();
 companyList();
 stringifyCompany();
+refreshEventHandler();
 
 
 //PRACTICE AREA FOR ADDING A "REFRESH" BUTTON
 
 //add a new event listener to a fresh button
-
-//html code that is needed for a submit button
-
-
-
-//refreshFormElement = document.getElementById('refreshForm');
-//refreshFormElement.addEventListener('submit', refreshEventHandler);
+var refreshFormElement = document.getElementById('refreshForm');
+refreshFormElement.addEventListener('submit', refreshEventHandler);
 
 
-//add a function to refresh form and turn the other event listener back on
-// function refreshEventHandler() {
-//  formElement.addEventListener('submit', eventHandler);
-//eventHandler();
-// }
+// add a function to refresh form and turn the other event listener back on
+function refreshEventHandler() {
+  formElement.addEventListener('submit', eventHandler);
+  eventHandler();
+}
 
 
 //display NEW results while keeping the OLD results
