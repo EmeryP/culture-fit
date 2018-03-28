a4d2b9ada772734f1dce07b0c025a11407df0df9
MADELINE PETERS <MADELINEPET@HOTMAIL.COM>
2018-03-28 11:08:46 -0700

    I currently have an alert to welcome a user back, but next I will make it more elegant by using JS to append the message to the html.

--- js/app.js
+++ js/app.js
@@ -1,38 +1,36 @@
 'use strict';
 
-//get el by id
 var formElement = document.getElementById('usernameForm');
 
-//store form input in to local storage(create function to accomplish this)
 function setupDataStorage(){
-//assigning the value of storedUsername the value of our key
   var storedUsername = localStorage.getItem('newUsername');
-  //there is a username
   if (storedUsername && storedUsername.length) {
-    alert('Hello ' + storedUsername);
+    alert('Welcome ' + storedUsername);
     console.log('Loaded from Local Storage');
-    //quit the function
     return;
   }
+  //do not do this here
+  // else{
+  //   alert('Please enter your name!');
+  // }
 }
 console.log('Doing it the hard way');
 
-// TODO: use form validation to confirm they enter some text
-// function to handle click event
 function getUserName(event){
   event.preventDefault();
   var newUsername = event.target.username.value;
+  if(newUsername && newUsername.length){
+    //if username has a length, say welcome back to user append an li to the ul with the content
+    alert('welcome ' + newUsername);
+  }
+
   storeData();
 }
 
-//TODO store the users name in a persons object(profile) so we can reference
-
-//Add event listener
 formElement.addEventListener('submit', getUserName);
 
-// Save to local storage on submit
 function storeData(){
-  localStorage.setItem('newUsername', event.target.username.value); //setting local storage to drill into event to set value
+  localStorage.setItem('newUsername', event.target.username.value);
 }
 
 setupDataStorage();
